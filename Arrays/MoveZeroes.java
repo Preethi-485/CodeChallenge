b12e251e32954847fd2350e755199128c07b796a
Preethi S <95564341+Preethi-485@users.noreply.github.com>
2023-12-11 17:50:39 +0530

    Add files via upload
    #2

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class MoveZeroes {
+
+    public static void main(String[] args){
+        int nums[]={0,0,1};
+        movezero(nums);
+    }
+
+    public static void movezero(int nums[]){
+       // int i=0;
+     /*   int z=0;
+      List<Integer> l=new ArrayList<>();
+      for(int i=0;i<nums.length;i++){
+          if(nums[i]==0)
+          z++;
+          else
+          l.add(nums[i]);
+      }
+      for(int i=0;i<nums.length-z;i++){
+          nums[i]=l.get(i);
+      }
+      for(int i=z;i<nums.length;i++)
+      nums[i]=0;*/
+      int inc=0;
+      for(int i=0;i<nums.length;i++){
+          if(nums[i]!=0){
+          nums[inc]=nums[i];
+          inc++;}
+      }
+      for(int i=inc;i<nums.length;i++)
+      nums[i]=0;
+      for(int i=0;i<nums.length;i++){
+
+        System.out.println(nums[i]);
+      }
+
+    
+}
+}
