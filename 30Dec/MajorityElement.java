e6f9e0e7722be472af1c13dc2db08fc7d513a452
Preethi-485 <preethisiva485@gmail.com>
2023-12-30 20:33:02 +0530

    leetcode

--- 30Dec/MajorityElement.java
+++ 30Dec/MajorityElement.java
@@ -1,2 +1,27 @@
 import java.util.*;
-class
+
+class MajorityElement {
+    
+    public static int majorityElement(int[] nums) {
+        int n=(int)Math.floor(nums.length/2);
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            int val=nums[i];
+            int c=1;
+            for(int j=i+1;j<nums.length;j++){
+                if(val==nums[j])
+                c++;
+            }
+            if(c>n)
+            return val;
+
+        }
+        return -1;
+        
+        
+    }
+    public static void main(String[] args) {
+        int nums[]={};
+        System.out.println(majorityElement(nums));
+    }
+}
