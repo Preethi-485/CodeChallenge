e956c6c3daa5b000c850cae8254a28a3717cf2ff
Preethi-485 <preethisiva485@gmail.com>
2023-12-30 13:14:30 +0530

    4

---
+++ 29Dec/PalindromeNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class PalindromeNumber{
+public static void main(String[] args){
+    int n=121;
+    System.out.println(pal(n));
+
+}
+public static boolean pal(int n){
+    int temp=n;
+    int sum=0;
+    while(n!=0){
+        int rem=n%10;
+        sum=(sum*10)+rem;
+        n=n/10;
+
+    }
+    return (temp==sum);
+   }
+}
